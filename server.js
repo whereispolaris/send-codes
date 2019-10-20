237efa269a5331682980950dcf193c3dd966ff29
Santiago Enciso <whereispolaris@gmail.com>
2019-10-20 01:13:33 -0500

    Added .env to gitignore file and contentful SDK snippets.

--- server.js
+++ server.js
@@ -2,6 +2,30 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+require('dotenv').config();
+
+
+// Contentful
+const contentful = require("contentful");
+
+const client = contentful.createClient({
+  space: process.env.CONTENTFUL_SPACE,
+  accessToken: process.env.CONTENTFUL_ACCESSTOKEN
+  // ,
+  // host: "preview.contentful.com"
+});
+
+// Get Articles from Contentful
+app.get("/api/articles", (req, res) => {
+  client.getEntries()
+    .then(response => {
+      res.json(response);
+    })
+    .catch((error) => {
+      res.send("error", error);
+    })
+})
+
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -10,10 +34,10 @@ if (process.env.NODE_ENV === "production") {
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
