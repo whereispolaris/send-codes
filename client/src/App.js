a2cee8c5b19731617d59f7a94cfd4f7050dec954
Santiago Enciso <whereispolaris@gmail.com>
2019-10-20 00:12:53 -0500

    Created Jumbotron component and added it to App.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Header from './components/Header';
+import Jumbo from './components/Jumbo';
 import "./App.css";
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div>
         <Header />
+        <Jumbo />
       </div>
     );
   }
---
+++ client/src/components/Jumbo/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Jumbo = () => {
+    return (
+        <div>
+            <div class="jumbotron jumbotron-fluid">
+                <div class="container">
+                    <h1 class="display-4 text-center">Send Codes</h1>
+                    <p class="lead text-center">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Jumbo;
