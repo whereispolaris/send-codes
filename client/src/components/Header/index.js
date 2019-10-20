7f36bb58a9df4d58ed4f0624202c76946245d582
Santiago Enciso <whereispolaris@gmail.com>
2019-10-19 23:30:55 -0500

    Created Header component.

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h3>Hello World</h3>
+            <p> JavaScript is more fun with React!</p>
+        </div>
+    )
+}
+
+export default Header;
