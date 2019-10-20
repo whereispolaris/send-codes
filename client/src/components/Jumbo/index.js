43b2b0c09cb804b3478b2a24b1f29cf791462149
Santiago Enciso <whereispolaris@gmail.com>
2019-10-20 00:13:42 -0500

    Updated classes to JSX's classNames.

--- client/src/components/Jumbo/index.js
+++ client/src/components/Jumbo/index.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Jumbo = () => {
     return (
         <div>
-            <div class="jumbotron jumbotron-fluid">
-                <div class="container">
-                    <h1 class="display-4 text-center">Send Codes</h1>
-                    <p class="lead text-center">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+            <div className="jumbotron jumbotron-fluid">
+                <div className="container">
+                    <h1 className="display-4 text-center">Send Codes</h1>
+                    <p className="lead text-center">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
                 </div>
             </div>
         </div>
