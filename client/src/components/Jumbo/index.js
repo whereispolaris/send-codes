d9d635a963a3f40bfb3c07f72ff0fe5e3612c355
Joe Hancock <joe.hancock95@gmail.com>
2019-10-24 17:04:38 -0500

    added styling and fonts

--- client/src/components/Jumbo/index.js
+++ client/src/components/Jumbo/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import './Jumbo.css'
+import './Jumbo.css';
 
 const Jumbo = () => {
     return (
         <div>
             <div className="jumbotron jumbotron-fluid">
                 <div className="container">
-                    <h1 className="display-4 text-center">Send Codes</h1>
+                    <h1 className="display-4 text-center">SEND CODES</h1>
                     <p className="lead text-center">Find boilerplate for your project.</p>
                 </div>
             </div>
