046e083a537f2b5c391e528c9bbb2e2e1d068e85
Santiago Enciso <whereispolaris@gmail.com>
2019-10-27 13:05:35 -0500

    Added BrowserRouter and Route components to App.js, created About component.

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Header from './components/Header';
 import Jumbo from './components/Jumbo';
 import ArticleSearch from './components/ArticleSearch';
+import About from './components/About';
 import "./App.css";
 import "./fonts/stylesheet.css";
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
@@ -10,13 +11,19 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Jumbo />
-      <div className="container">
-        <ArticleSearch />
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Jumbo />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={ArticleSearch} />
+            <Route exact path="/about" component={About} />
+            {/* <Route component={NotFound} /> */}
+          </Switch>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import Header from '../Header';
 
 const About = () => {
     return (
         <div>
-            <Header />
             <h3>About Page</h3>
             <p>JavaScript is more fun with React!</p>
         </div>
