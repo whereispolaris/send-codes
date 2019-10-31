7ffb59737f9e70c5b8b6c667865ec15840e10618
Santiago Enciso <whereispolaris@gmail.com>
2019-10-30 20:55:07 -0500

    Removed extra className props from <li> element.

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -8,21 +8,21 @@ const Header = () => {
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg justify-content-between">
                 <ul className="navbar-nav mr-auto">
-                    <li className="nav-item" className="linkstr">
-                        <Link className="nav-link" to="/"><img className="main-logo" src={logo} /></Link>
+                    <li className="nav-item linkstr">
+                        <Link className="nav-link" to="/"><img className="main-logo" src={logo} alt="Send Codes Logo"/></Link>
                     </li>
                 </ul>
                 <ul className="navbar-nav ml-auto">
-                    <li className="nav-item" className="linkstr">
+                    <li className="nav-item linkstr">
                         <Link className="nav-link" to="/testarticle">Test Article</Link>
                     </li>
-                    <li className="nav-item" className="linkstr">
+                    <li className="nav-item linkstr">
                         <Link className="nav-link" to="/about">About</Link>
                     </li>
-                    <li className="nav-item" className="linkstr">
+                    <li className="nav-item linkstr">
                         <Link className="nav-link" to="/login">Login</Link>
                     </li>
-                    <li className="nav-item" className="linkstr">
+                    <li className="nav-item linkstr">
                         <Link className="nav-link" to="/contribute">Contribute</Link>
                     </li>
                 </ul>
