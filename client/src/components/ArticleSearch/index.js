c33cd730bd5f9a2fd7f66a34a3d5e6d9dcf2410f
Lorena Ontiveros <lorena0328o@gmail.com>
2019-10-31 20:49:14 -0500

    we can filter based off of selected tags on the drop down menu

--- client/src/components/ArticleSearch/index.js
+++ client/src/components/ArticleSearch/index.js
@@ -10,14 +10,20 @@ const ArticleSearch = () => {
 
     const [articles, setArticle] = useState([]);
     const [load, setLoad] = useState(false);
-
+    const [subject, setSubject] = useState("general");
     useEffect(() => {
         axios.get("/api/blogs").then(response => {
+            console.log( response.data.items)
             setArticle(response.data.items);
             setLoad(true);
         })
     }, []);
-
+const handleChange = (e) => {
+    console.log(e.currentTarget.textContent)
+    setSubject(e.currentTarget.textContent.toLowerCase())
+    console.log(subject)
+}
+console.log(subject)
     if (load) {
         return (
             <div>
@@ -27,15 +33,31 @@ const ArticleSearch = () => {
                             <ul className="nav nav-pills nav-fill">
                                 <li className="nav-item dropdown">
                                     <button id="dropDownButton" className="nav-link dropdown-toggle" data-toggle="dropdown" href="#" aria-haspopup="true" aria-expanded="false">Popular</button>
-                                    <div className="dropdown-menu">
-                                        <a className="dropdown-item" href="/">JavaScript</a>
-                                        <a className="dropdown-item" href="/">HTML</a>
-                                        <a className="dropdown-item" href="/">CSS</a>
-                                        <a className="dropdown-item" href="/">PHP</a>
-                                        <a className="dropdown-item" href="/">React</a>
-                                        <a className="dropdown-item" href="/">Bootstrap</a>
-                                        <a className="dropdown-item" href="/">Laravel</a>
-                                        <a className="dropdown-item" href="/">Express</a>
+                                    <div className="dropdown-menu" >
+                                        <a id="dd1" className="dropdown-item" onClick = {
+                                            handleChange
+                                        } >JavaScript</a>
+                                        <a id="dd2" className="dropdown-item" onClick = {
+                                            handleChange
+                                        }>HTML</a>
+                                        <a id="dd3" className="dropdown-item" onClick = {
+                                            handleChange
+                                        }>CSS</a>
+                                        <a id="dd4" className="dropdown-item" onClick = {
+                                            handleChange
+                                        }>PHP</a>
+                                        <a id="dd5" className="dropdown-item" onClick = {
+                                            handleChange
+                                        }>React</a>
+                                        <a id="dd6" className="dropdown-item" onClick = {
+                                            handleChange
+                                        }>Bootstrap</a>
+                                        <a id="dd7" className="dropdown-item" onClick = {
+                                            handleChange
+                                        }>Laravel</a>
+                                        <a id="dd8" className="dropdown-item" onClick = {
+                                            handleChange
+                                        }>Express</a>
                                     </div>
                                 </li>
                             </ul>
@@ -47,8 +69,11 @@ const ArticleSearch = () => {
                     </div>
                 </form>
                 {
-                    articles.map((article, index) => (
-                        <Article
+                    articles.map((article, index) => {
+                        if (
+                            article.fields.tags.indexOf(subject)!=-1
+                        ) { return (
+                    <Article
                             key={article.sys.id}
                             // image="https://picsum.photos/id/1073/100/100"
                             title={article.fields.title}
@@ -58,7 +83,10 @@ const ArticleSearch = () => {
                             // isSaved="Function"
                             articleLink="https://picsum.photos/id/1073/100/100"
                         />
-                    ))
+                        )
+                        }
+                        
+                })
                 }
             </div>
         )
