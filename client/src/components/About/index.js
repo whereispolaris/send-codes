bb2102e142bda02fc3bb5261995378b97951fd7d
Joe Hancock <joe.hancock95@gmail.com>
2019-11-02 18:20:25 -0700

    added developers in alphabetical order

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -8,6 +8,11 @@ const About = () => {
             <p>This project is developed by web developers for web developers with templates and code snippets to facilitate learning.
                 Send-Codes is built to simplify boilerplates and access useful commands in a simple straight forward manner.
             </p>
+            <h3>Project Devlopers</h3>
+            <h4>Santiago Enciso</h4>
+            <h4>Joseph Hancock</h4>
+            <h4>Suresh Mahato</h4>
+            <h4>Lorena Ontiveros</h4>
         </div>
     )
 }
