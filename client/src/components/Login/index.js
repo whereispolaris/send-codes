2bbf92e3fc4cb378be7bec4dad354be2477768ae
Santiago Enciso <whereispolaris@gmail.com>
2019-11-05 19:56:07 -0600

    Removed username and password labels from login page.

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -56,9 +56,6 @@ class LoginForm extends Component {
                     <h4>Login</h4>
                     <form className="form-horizontal">
                         <div className="form-group">
-                            <div className="col-1 col-ml-auto">
-                                <label className="form-label" htmlFor="username">Username</label>
-                            </div>
                             <div className="col-3 col-mr-auto">
                                 <input className="form-input"
                                     type="text"
@@ -71,9 +68,6 @@ class LoginForm extends Component {
                             </div>
                         </div>
                         <div className="form-group">
-                            <div className="col-1 col-ml-auto">
-                                <label className="form-label" htmlFor="password">Password: </label>
-                            </div>
                             <div className="col-3 col-mr-auto">
                                 <input className="form-input"
                                     placeholder="password"
