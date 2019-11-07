efacf2b4f1ca502a4b9fd114387ff99db318e476
Joe Hancock <joe.hancock95@gmail.com>
2019-11-06 21:25:04 -0600

    query is being handled via onchange, switching branches. stashing

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+// import './Search.css';
 import ArticlePage from '../ArticlePage';
 import Article from '../Article';
 const axios = require('axios');
 const matter = require('gray-matter');
+
 
 class Search extends React.Component {
     constructor( props ) {
